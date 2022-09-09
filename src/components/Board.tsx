a6ecab58f5c7ce68797356100e9dfc9046bbfb0a
Oktay <oktay@yatko.dev>
2022-09-09 22:07:15 +0200

    Add constraint ref for pieces in board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,27 +2,36 @@ import { Board as TBoard, createPosition, Files, Ranks } from "../lib";
 import Square from "./Square";
 import styles from "./Board.module.css";
 import Piece from "./Piece";
+import { createContext, RefObject, useRef } from "react";
 
 export interface BoardProps {
   board?: TBoard;
 }
 
+export const DragConstraintRefContext = createContext<
+  RefObject<HTMLDivElement>
+>(null as unknown as RefObject<HTMLDivElement>);
+
 export default function Board(props: BoardProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
   return (
-    <div>
-      {Ranks.slice()
-        .reverse()
-        .map((rank, i) => (
-          <div key={i} className={styles.row}>
-            {Files.map((file, i) => (
-              <Square key={i} rank={rank} file={file}>
-                {props.board?.[createPosition(file, rank)] && (
-                  <Piece piece={props.board?.[createPosition(file, rank)]!} />
-                )}
-              </Square>
-            ))}
-          </div>
-        ))}
+    <div ref={ref} className={styles.board}>
+      <DragConstraintRefContext.Provider value={ref}>
+        {Ranks.slice()
+          .reverse()
+          .map((rank, i) => (
+            <div key={i} className={styles.row}>
+              {Files.map((file, i) => (
+                <Square key={i} rank={rank} file={file}>
+                  {props.board?.[createPosition(file, rank)] && (
+                    <Piece piece={props.board?.[createPosition(file, rank)]!} />
+                  )}
+                </Square>
+              ))}
+            </div>
+          ))}
+      </DragConstraintRefContext.Provider>
     </div>
   );
 }
--- src/components/Piece.tsx
+++ src/components/Piece.tsx
@@ -1,13 +1,16 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useContext } from "react";
 import { Piece as TPiece, PieceKind } from "../lib";
 import { motion } from "framer-motion";
 import styles from "./Piece.module.css";
+import { DragConstraintRefContext } from "./Board";
 
 export interface PieceProps {
   piece: TPiece;
 }
 
 export default function Piece(props: PieceProps) {
+  const ref = useContext(DragConstraintRefContext);
+
   return (
     <motion.div
       className={styles.piece}
@@ -18,6 +21,8 @@ export default function Piece(props: PieceProps) {
         bounceStiffness: 200,
         bounceDamping: 30,
       }}
+      dragConstraints={ref}
+      dragElastic={0.1}
       style={
         {
           ["--piece-offset"]: props.piece.kind,
