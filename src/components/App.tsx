45f1d08ee113d7c399739c67dad94cbc2da3ee88
Oktay <oktay@yatko.dev>
2022-09-09 19:10:16 +0200

    Add scale slider

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
+import { CSSProperties, useState } from "react";
 import { initBoard } from "../lib";
 import Board from "./Board";
 
 export default function App() {
+  const [scale, setScale] = useState(1.5);
+
   return (
-    <div>
+    <div id="app" style={{ ["--scale"]: scale } as CSSProperties}>
+      <input
+        type="range"
+        min="1"
+        max="4"
+        step="0.01"
+        value={scale}
+        onChange={(e) => setScale(e.target.value as unknown as number)}
+      />
       <Board board={initBoard()} />
     </div>
   );
